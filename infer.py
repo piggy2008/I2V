943f6b33a5765bb37ae60e35155e92aaeae0c549
piggy2008 <810656271@qq.com>
2020-12-08 21:41:10 +0800

    ensemble simply plus

--- infer.py
+++ infer.py
@@ -28,7 +28,7 @@ ckpt_path = './ckpt2'
 exp_name = 'VideoSaliency_2020-12-08 18:42:03'
 
 args = {
-    'snapshot': '8000',  # your snapshot filename (exclude extension name)
+    'snapshot': '10000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
