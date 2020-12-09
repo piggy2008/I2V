7a9591268106ca1dca686229630c4da1b6ae0a90
piggy2008 <810656271@qq.com>
2020-12-09 10:04:47 +0800

    2 models plus

--- infer.py
+++ infer.py
@@ -28,7 +28,7 @@ ckpt_path = './ckpt2'
 exp_name = 'VideoSaliency_2020-12-08 22:10:01'
 
 args = {
-    'snapshot': '10000',  # your snapshot filename (exclude extension name)
+    'snapshot': '5000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (350, 350)
