ffa9209e5dd5b88cd5efdf858f37057e832debb3
piggy2008 <810656271@qq.com>
2020-12-09 14:22:46 +0800

    remove output plus, add contrast with l2

--- infer.py
+++ infer.py
@@ -25,10 +25,10 @@ torch.cuda.set_device(device_id)
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt2'
-exp_name = 'VideoSaliency_2020-12-08 22:10:01'
+exp_name = 'VideoSaliency_2020-12-09 10:31:05'
 
 args = {
-    'snapshot': '5000',  # your snapshot filename (exclude extension name)
+    'snapshot': '8000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (350, 350)
