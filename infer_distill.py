35ad3e468d76165799dc983e67cbbe3b2d6f2846
piggy2008 <810656271@qq.com>
2020-12-10 09:59:04 +0800

    just finetune

--- infer_distill.py
+++ infer_distill.py
@@ -35,7 +35,7 @@ exp_name = 'VideoSaliency_2020-12-09 20:05:42'
 
 args = {
     'model': 'F3Net',
-    'snapshot': '4000',  # your snapshot filename (exclude extension name)
+    'snapshot': '20000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
