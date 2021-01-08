e8fa61447f449b5ef0039582c58caedd07087562
piggy2008 <810656271@qq.com>
2021-01-08 20:02:26 +0800

    update

--- infer_distill.py
+++ infer_distill.py
@@ -31,11 +31,11 @@ torch.cuda.set_device(device_id)
 # the following two args specify the location of the file of trained model (pth extension)
 # you should have the pth file in the folder './$ckpt_path$/$exp_name$'
 ckpt_path = './ckpt'
-exp_name = 'VideoSaliency_2020-09-08 08:46:47'
+exp_name = 'VideoSaliency_2020-07-21 13:01:22'
 
 args = {
-    'model': 'F3Net',
-    'snapshot': '2000',  # your snapshot filename (exclude extension name)
+    'model': 'R3Net',
+    'snapshot': '120000',  # your snapshot filename (exclude extension name)
     'crf_refine': False,  # whether to use crf to refine results
     'save_results': True,  # whether to save the resulting masks
     'input_size': (380, 380)
